0062df6f27941d96df2117d744188d1132c1fa33
Thanawat <guitardev@admins-MacBook-Pro.local>
2022-09-11 10:30:57 +0700

    ep3 Asynv Await

--- 21day/d3/async-await.js
+++ 21day/d3/async-await.js
@@ -6,21 +6,59 @@
 // simulateAsyncAPI('A',1000);
 // simulateAsyncAPI('B',500);
 // simulateAsyncAPI('C',100);
-function simulateAsyncAPI(text, timeout, callback){
-    setTimeout(()=>{
-        console.log(text)
-        if (callback){
-        callback();}
-    }, timeout);
-}
+// function simulateAsyncAPI(text, timeout, callback){
+//     setTimeout(()=>{
+//         console.log(text)
+//         if (callback){
+//         callback();}
+//         // callback();
+//     }, timeout);
+// }
 // Callback Hell
 // simulateAsyncAPI('A',1000,()=>{
 //     simulateAsyncAPI('B',500,()=>{
 //         simulateAsyncAPI('C',100);
-
 //     });
 // });
+// 3.Promise
+// function simulateAsyncAPI(text,timeout){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(() => {
+//             if (text === 'C') return reject('C got error')
+//             console.log(text);
+//             resolve();
+//         }, timeout);
+//     });
+// }
+// simulateAsyncAPI('A',1000)
+//     .then(()=> {
+//         return simulateAsyncAPI('B',500)
+//     })
+//     .then(() => {
+//         return simulateAsyncAPI('C',1000)
+//     })
+//     .catch((error) => {
+//         console.error(error);
+//     });
 
+function simulateAsyncAPI(text,timeout){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            if (text === 'C') return reject('C got error')
+            console.log(text);
+            resolve();
+        }, timeout);
+    });
+}
 
+async function run(){
+    try {
+    await simulateAsyncAPI('A',1000);
+    await simulateAsyncAPI('B',2500);
+    await simulateAsyncAPI('C',5000);
+} catch (error){
+    console.error(error)
+}}
 
+run();
 })(); 
